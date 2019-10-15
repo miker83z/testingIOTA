296f470f1a2531ca95365031d72519a5528629c5
Mirko <mikerz83@gmail.com>
2019-10-15 13:09:47 +0200

    1.0

--- IOTAScript.js
+++ IOTAScript.js
@@ -15,16 +15,13 @@ const ccurl = require('ccurl.interface.js');
 
 // Command line arguments
 const optionDefinitions = [
-  { name: 'keepinitial', alias: 'k', type: Boolean, defaultValue: true },
-  { name: 'notmam', alias: 'n', type: Boolean, defaultValue: false },
+  { name: 'keepinitial', alias: 'k', type: Boolean, defaultValue: false },
+  { name: 'single', alias: 's', type: Boolean, defaultValue: false },
   { name: 'random', alias: 'r', type: Boolean, defaultValue: false },
   { name: 'localpow', alias: 'l', type: Boolean, defaultValue: false },
-  { name: 'showpow', alias: 'p', type: Boolean, defaultValue: false },
   { name: 'devnet', alias: 'd', type: Boolean, defaultValue: false },
-  { name: 'single', alias: 't', type: Boolean, defaultValue: false },
-  { name: 'mul', alias: 'x', type: Number, defaultValue: 3 },
   { name: 'iter', alias: 'i', type: Number, defaultValue: 1 },
-  { name: 'slice', alias: 's', type: Number, defaultValue: 20 },
+  { name: 'window', alias: 'w', type: Number, defaultValue: 10 },
   {
     name: 'ccurl',
     alias: 'c',
@@ -36,23 +33,22 @@ const commandLineArgs = require('command-line-args');
 const options = commandLineArgs(optionDefinitions);
 
 // Path defining attributes
-const KEEPINITIALP = options.keepinitial;
-let ISNOTMAM = options.notmam;
-const ISRANDOM = options.random;
+let KEEPINITIALP = options.keepinitial;
+let ISSINGLE = options.single;
+let ISRANDOM = options.random;
 const ISLOCALPOW = options.localpow;
-let SHOWPOW = options.showpow;
-if (ISLOCALPOW) SHOWPOW = true;
 const ISDEVNET = options.devnet;
-let multiplier = options.mul;
-if (options.single) {
-  ISNOTMAM = true;
-  multiplier = 1;
+if (ISDEVNET) {
+  KEEPINITIALP = true;
+  ISRANDOM = false;
 }
 const iterations = options.iter ? options.iter : 1;
-const sliceValue = options.slice ? options.slice : 20;
+const providersWindow = options.window ? options.window : 10;
 const ccurlpath = options.ccurl;
 
 // Constant Values
+let mwm = ISDEVNET ? 9 : 14; //minimum weight magnitude
+const depth = 3; //depth
 const devnetProv = {
   hostname: 'https://nodes.devnet.thetangle.org:443',
   score: 1
@@ -71,21 +67,16 @@ const busConst = [
   '484',
   '422'
 ];
-let iotaProviders, bus, latestMilestones, bestScore, messageX, previousFetch;
+let iotaProviders, bus, latestMilestones, bestScore, previousFetch;
 
 const setupEnvironment = () => {
   iotaProviders = [];
   bus = {};
   latestMilestones = [];
   bestScore = 1;
-  if (ISNOTMAM) {
-    messageX = '';
-    for (let i = 0; i < 95 * multiplier; i++) {
-      messageX += 'Hello IOTA ';
-    }
-  }
   previousFetch = 0;
 };
+
 const iotaSeedGen = key => {
   const rng = seedrandom(key);
   const iotaSeedLength = 81;
@@ -160,7 +151,7 @@ const setupProviders = async () => {
     return b - a;
   });
   iotaProviders = iotaProviders.filter(e => {
-    return e.latestMil !== latestMilestones[0];
+    return e.latestMil === latestMilestones[0];
   });
   if (KEEPINITIALP) {
     // Order by score and pick the best score
@@ -172,13 +163,13 @@ const setupProviders = async () => {
     bestScore = iotaProviders[0].score;
     // Pick the best n (if not random choice)
     if (!ISRANDOM) {
-      iotaProviders = iotaProviders.slice(0, sliceValue);
+      iotaProviders = iotaProviders.slice(0, providersWindow);
       shuffle(iotaProviders, { rng: seedrandom() });
     }
   }
 };
 
-const selectProvider = x => {
+const selectDifferentProvider = x => {
   return iotaProviders[x % iotaProviders.length];
 };
 
@@ -213,6 +204,33 @@ const selectBestKnownProvider = b => {
   return orderedBestProviders[0][0]; // Should never occur
 };
 
+const chooseProviderAlgorithm = async b => {
+  let p = bus[b].currentProvider;
+  if (ISRANDOM) {
+    p = (await fetchRandomProvider()).hostname;
+  } else {
+    // Choose the provider to use
+    // If current provider is in "waiting" for an attachment process, choose another one
+    if (bus[b].waiting.has(bus[b].currentProvider)) {
+      // If there are known providers not waiting, choose the best one
+      if (bus[b].waiting.size < Object.keys(bus[b].providersRTT).length)
+        p = selectBestKnownProvider(b).hostname;
+      // If every known provider is waiting for an attachment, search for a new one
+      else {
+        // If the provider found is aready known, search for a new one
+        while (
+          Object.keys(bus[b].providersRTT).includes(
+            (p = (await fetchRandomProvider()).hostname)
+          )
+        );
+        // Add the new provider to the known
+        bus[b].providersRTT[p] = -1;
+      }
+    }
+  }
+  return p;
+};
+
 // Initial phase, creating log files and opening MAM channels
 const init = async () => {
   try {
@@ -220,53 +238,43 @@ const init = async () => {
     let dirTemp = 'dataset/';
     if (KEEPINITIALP) dirTemp += 'keep/';
     else dirTemp += 'keep-NOT/';
-    if (ISNOTMAM) dirTemp += 'mam-NOT/';
+    if (ISSINGLE) dirTemp += 'single/';
     else dirTemp += 'mam/';
     if (ISRANDOM) dirTemp += 'random/';
     else dirTemp += 'random-NOT/';
-    if (multiplier === 1) dirTemp += 'single';
-    else if (ISNOTMAM) dirTemp += 'triple';
-    else dirTemp += 'data';
 
+    dirTemp += 'data';
     if (ISLOCALPOW) dirTemp += '-LOCAL';
-    if (SHOWPOW && ISNOTMAM) dirTemp += '-POW';
     if (ISDEVNET) dirTemp += '-DEVNET';
-
     if (!fs.existsSync(dirTemp)) fs.mkdirSync(dirTemp);
     const dir = dirTemp + '/' + new Date().toISOString();
 
     if (ISDEVNET) iotaProviders.push(devnetProv);
     else await setupProviders();
 
-    if (ISLOCALPOW) MAM.setAttachToTangle(localAttachToTangle);
-
     // For each bus setup a MAM channel or IOTA api, then create a log file
     for (let i = 0; i < busConst.length; i++) {
-      const seed = iotaSeedGen();
       // Provider
-      let provider = null;
-      if (ISRANDOM || !KEEPINITIALP) provider = selectRandomProvider();
-      else provider = selectProvider(i);
+      const provider =
+        ISRANDOM || !KEEPINITIALP
+          ? selectRandomProvider()
+          : selectDifferentProvider(i);
       // Bus object
       bus[busConst[i]] = {
         channel: null,
         csv: null,
-        seed,
-        currentProvider: provider,
+        currentProvider: provider.hostname,
         providersRTT: {},
         waiting: new Set()
       };
       bus[busConst[i]].providersRTT[provider] = -1;
 
-      // Setup MAM Channel or IOTA api
-      let tempChannel = null;
-      if (ISNOTMAM) {
-        tempChannel = IOTA.composeAPI({ provider: provider.hostname });
-        if (ISLOCALPOW) tempChannel.attachToTangle = localAttachToTangle;
-      } else {
-        tempChannel = MAM.changeMode(MAM.init(tempProvider, seed), 'private');
-      }
-      bus[busConst[i]].channel = tempChannel;
+      // Setup MAM Channel
+      if (!ISSINGLE)
+        bus[busConst[i]].channel = MAM.changeMode(
+          MAM.init(provider.hostname, iotaSeedGen()),
+          'private'
+        );
 
       // Create log file
       if (!fs.existsSync(dir)) fs.mkdirSync(dir);
@@ -280,144 +288,129 @@ const init = async () => {
           provider.score +
           ' scoreNorm: ' +
           provider.score / bestScore +
-          (!ISNOTMAM ? ' ' + MAM.getRoot(tempChannel) : '') +
+          (!ISSINGLE ? ' ' + MAM.getRoot(bus[busConst[i]].channel) : '') +
           '\n',
         err => {
           if (err) throw err;
         }
       );
+      sleep(50);
     }
   } catch (error) {
     console.log('SETUP ERROR: ' + error);
   }
 };
 
-// Publishing transactions on IOTA
-const publish = async row => {
-  let startTime = -1,
-    tipsObtainedTime = -1,
-    attachmentTime = -1,
-    finishTime = -1;
-  const transfers = [
+const prepareMAMMessage = (b, json) => {
+  const trytes = converter.asciiToTrytes(JSON.stringify(json));
+  const message = MAM.create(bus[b].channel, trytes);
+  bus[b].channel = message.state;
+  return [
     {
-      address: iotaSeedGen('recipient9999999kh' + new Date()),
+      address: message.address,
       value: 0,
-      tag: '',
-      message: converter.asciiToTrytes(
-        JSON.stringify({
-          payload: messageX,
-          timestamp: new Date().getTime()
-        })
-      )
+      message: message.payload,
+      tag: ''
     }
   ];
-  try {
-    startTime = new Date().getTime();
-    // Prepare a bundle and signs it.
-    const trytes = await bus[row[1]].channel.prepareTransfers(
-      bus[row[1]].seed,
-      transfers
-    );
-    // Does tip selection, attaches to tangle by doing PoW and broadcasts.
-    //const bundle = await bus[row[1]].channel.sendTrytes(trytes, 3, 14);
-    const { trunkTransaction, branchTransaction } = await bus[
-      row[1]
-    ].channel.getTransactionsToApprove(3);
-    tipsObtainedTime = new Date().getTime();
-
-    const attachedTrytes = await bus[row[1]].channel.attachToTangle(
-      trunkTransaction,
-      branchTransaction,
-      14,
-      trytes
-    );
-    await bus[row[1]].channel.storeAndBroadcast(attachedTrytes);
-    const bundle = attachedTrytes.map(t => tconverter.asTransactionObject(t));
-    finishTime = new Date().getTime();
-    attachmentTime = bundle[0].attachmentTimestamp;
-
-    // Compute latency
-    if (SHOWPOW)
-      console.log(
-        'bus ' +
-          row[1] +
-          ': tips ' +
-          (tipsObtainedTime - startTime) +
-          'ms, pow ' +
-          (finishTime - tipsObtainedTime) +
-          'ms'
-      );
-    else
-      console.log(
-        'bus ' +
-          row[1] +
-          ': ' +
-          (attachmentTime - startTime) +
-          ' ms, ' +
-          (finishTime - startTime) +
-          ' ms'
-      );
-  } catch (err) {
-    console.log(row[1] + ': ' + err);
-  } finally {
-    fs.appendFile(
-      bus[row[1]].csv,
-      startTime +
-        ',' +
-        (SHOWPOW ? tipsObtainedTime + ',' : '') +
-        attachmentTime +
-        ',' +
-        finishTime +
-        ',' +
-        row[4] +
-        '\n',
-      err => {
-        if (err) throw err;
-      }
-    );
-  }
 };
 
-// Publishing a message json on a channel
-const publishOnMAM = async (row, json) => {
-  let minWeightMagn = 14;
-  if (ISDEVNET) minWeightMagn = 9;
-  let startTime = -1,
-    attachmentTime = -1,
-    finishTime = -1;
+const prepareSingleTX = json => {
+  return [
+    {
+      address: iotaSeedGen('r9999999' + new Date()),
+      value: 0,
+      message: converter.asciiToTrytes(JSON.stringify(json)),
+      tag: ''
+    }
+  ];
+};
+
+// Publishing a message json on a MAM channel or as a single TX
+const publish = async (b, id, json) => {
+  let startTS = -1,
+    tipsTS = -1,
+    finishTS = -1,
+    provider = bus[b].currentProvider;
   try {
+    if (!KEEPINITIALP) {
+      provider = await chooseProviderAlgorithm(b);
+      bus[b].currentProvider = provider;
+      bus[b].waiting.add(provider);
+    }
+
     // Prepare message
-    const trytes = converter.asciiToTrytes(JSON.stringify(json));
-    const message = MAM.create(bus[row[1]].channel, trytes);
-    bus[row[1]].channel = message.state;
+    const transfers = ISSINGLE
+      ? prepareSingleTX(json)
+      : prepareMAMMessage(b, json);
 
-    // Attach the payload to the channel
-    startTime = new Date().getTime();
-    const bundle = await MAM.attach(
-      message.payload,
-      message.address,
-      3,
-      minWeightMagn
+    // Prepare API and transaction
+    const iota = IOTA.composeAPI({ provider });
+    const attachToTangle = ISLOCALPOW
+      ? localAttachToTangle
+      : iota.attachToTangle;
+    const trytes = await iota.prepareTransfers('9'.repeat(81), transfers, {});
+
+    //Start operations
+    startTS = new Date().getTime();
+    // Tip selection
+    const {
+      trunkTransaction,
+      branchTransaction
+    } = await iota.getTransactionsToApprove(depth);
+    tipsTS = new Date().getTime();
+    // Attaches to tangle by doing PoW and broadcasts.
+    const attachedTrytes = await attachToTangle(
+      trunkTransaction,
+      branchTransaction,
+      mwm,
+      trytes
     );
-    finishTime = new Date().getTime();
-    attachmentTime = bundle[0].attachmentTimestamp;
+    finishTS = new Date().getTime();
+    await iota.storeAndBroadcast(attachedTrytes);
+    const bundle = attachedTrytes.map(t => tconverter.asTransactionObject(t));
 
-    // Compute latency
+    // Latency measures
+    r = finishTS - startTS;
+    tps = tipsTS - startTS;
+    pw = finishTS - tipsTS;
+
+    if (!KEEPINITIALP) {
+      bus[b].waiting.delete(provider);
+      // Compute latency
+      tmpRTT = bus[b].providersRTT[provider];
+      if (tmpRTT > 0)
+        bus[b].providersRTT[provider] = Math.ceil(
+          tmpRTT - alpha * (tmpRTT - r)
+        );
+      else bus[b].providersRTT[provider] = r;
+    }
+
+    // Log result
     console.log(
-      'bus ' +
-        row[1] +
-        ': ' +
-        (attachmentTime - startTime) +
-        ' ms, ' +
-        (finishTime - startTime) +
-        ' ms'
+      'bus ' + b + ': tips ' + tps + 'ms, pow ' + pw + 'ms, ' + provider
     );
   } catch (err) {
-    console.log(row[1] + ': ' + err);
+    console.log(b + ': ' + err);
+    if (!KEEPINITIALP) {
+      bus[b].waiting.delete(provider);
+      // Compute latency
+      tmpRTT = bus[b].providersRTT[provider];
+      if (tmpRTT > 0) bus[b].providersRTT[provider] = 10 * tmpRTT;
+      else bus[b].providersRTT[provider] = 100;
+    }
   } finally {
     fs.appendFile(
-      bus[row[1]].csv,
-      startTime + ',' + attachmentTime + ',' + finishTime + ',' + row[4] + '\n',
+      bus[b].csv,
+      startTS +
+        ',' +
+        tipsTS +
+        ',' +
+        finishTS +
+        ',' +
+        id +
+        (!KEEPINITIALP ? ',' + provider : '') +
+        '\n',
       err => {
         if (err) throw err;
       }
@@ -434,12 +427,10 @@ const go = async () => {
       console.log('Waiting ' + row[0] + ' seconds for bus ' + row[1]);
       await sleep(parseInt(row[0]) * 1000);
 
-      if (ISNOTMAM) publish(row);
-      else
-        publishOnMAM(row, {
-          payload: { latitude: row[2], longitude: row[3] },
-          timestampISO: new Date().toISOString()
-        });
+      publish(row[1], row[4], {
+        payload: { latitude: row[2], longitude: row[3] },
+        timestampISO: new Date().toISOString()
+      });
     }
   } catch (error) {
     console.log(error);
